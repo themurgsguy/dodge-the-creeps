deffe301f60f044b27d75e715ee0caf63d87e422
themurgsguy <ernest.da.cruz@gmail.com>
2019-09-22 01:38:45 +0100

    Initial commit

---
+++ Mob.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+export var min_speed = 150
+export var max_speed = 250
+var mob_types = ["walk", "swim", "fly"]
+
+
+func _ready():
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	$AnimatedSprite.playing = true
+
+
+func _on_start_game():
+	queue_free()
+
+
+func _on_Visibility_screen_exited():
+	queue_free()
